93eb79d4557cb802e1c03f3c9a6cddc0dc249e27
Rodrigo <rodrigo100snt@gmail.com>
2023-04-04 00:01:38 -0300

    Add script

---
+++ script.js
@@ -0,0 +1,20 @@
+// FOR
+var i;
+for (i = 0; i < 5; i++) {
+  // Executa 5 vezes, com os valores de passos de 0 a 4.
+  console.log('esta no numero: ' + i);
+}
+
+// WHILE
+n = 0;
+while (n < 5) {
+  n++;
+  console.log('esta no numero: ' + n);
+}
+
+// DO WHILE
+i = 0;
+do {
+  i += 1;
+  console.log('esta no numero: ' + i);
+} while (i < 5);
